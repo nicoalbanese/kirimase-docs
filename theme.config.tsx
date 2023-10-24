66e377aa4fb80bb3f501566dc07322a6c1737fe9
Nico Albanese <gcalbanese96@gmail.com>
2023-10-24 10:52:51 +0100

    added favicons

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,4 +1,5 @@
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: (
@@ -20,6 +21,51 @@ const config: DocsThemeConfig = {
   docsRepositoryBase: "https://github.com/nicoalbanese/kirimase-docs",
   footer: {
     text: "Kirimase Docs Template",
+  },
+  primaryHue: 124,
+  primarySaturation: 38,
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – Kirimase",
+    };
+  },
+  head: () => {
+    const { frontMatter, title } = useConfig();
+
+    const ogTitle = title ? `${title} – Kirimase` : "No title";
+    const ogDescription = frontMatter.description;
+
+    return (
+      <>
+        {/* Favicons, meta */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="apple-mobile-web-app-title" content="SWR" />
+        <meta name="description" content={ogDescription} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@vercel" />
+        <meta property="og:title" content={ogTitle} />
+        <meta property="og:description" content={ogDescription} />
+      </>
+    );
   },
 };
 
